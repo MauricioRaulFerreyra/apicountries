dfecc260877bcc453643081923fc7c62b5552ed2
MauricioRaulFerreyra <mauriciotauro82@hotmail.com>
2022-05-25 15:21:36 -0300

    prueba deploy con un get

--- index.js
+++ index.js
@@ -23,56 +23,60 @@ app.use('/', routes)
 
 let API = process.env.apiAll
 
-const getAll = async () => {
-  try {
-    let response = await axios(API)
-    response = response.data.map(res => {
-      return {
-        id: res.cca3,
-        name: res.name.common && res.name.common,
-        image: res.flags && res.flags.map(flag => flag),
-        continent: res.continents && res.continents.map(el => el),
-        capital: res.capital ? res.capital.map(el => el) : ['no data'],
-        subregion: res.subregion,
-        area: res.area,
-        population: res.population
-      }
-    })
+// const getAll = async () => {
+//   try {
+//     let response = await axios(API)
+//     response = response.data.map(res => {
+//       return {
+//         id: res.cca3,
+//         name: res.name.common && res.name.common,
+//         image: res.flags && res.flags.map(flag => flag),
+//         continent: res.continents && res.continents.map(el => el),
+//         capital: res.capital ? res.capital.map(el => el) : ['no data'],
+//         subregion: res.subregion,
+//         area: res.area,
+//         population: res.population
+//       }
+//     })
 
-    return response
-  } catch (err) {
-    console.log(err)
-  }
-}
+//     return response
+//   } catch (err) {
+//     console.log(err)
+//   }
+// }
 
-const getAllDb = async () => {
-  try {
-    let response = await getAll()
-    response.map(el => {
-      Country.findOrCreate({
-        where: { name: el.name },
-        defaults: {
-          id: el.id,
-          name: el.name,
-          image: el.image,
-          continent: el.continent,
-          capital: el.capital,
-          subregion: el.subregion,
-          area: el.area,
-          population: el.population
-        }
-      })
-    })
-    let data = await Country.findAll()
+// const getAllDb = async () => {
+//   try {
+//     let response = await getAll()
+//     response.map(el => {
+//       Country.findOrCreate({
+//         where: { name: el.name },
+//         defaults: {
+//           id: el.id,
+//           name: el.name,
+//           image: el.image,
+//           continent: el.continent,
+//           capital: el.capital,
+//           subregion: el.subregion,
+//           area: el.area,
+//           population: el.population
+//         }
+//       })
+//     })
+//     let data = await Country.findAll()
 
-    return data
-  } catch (e) {
-    console.log(e)
-  }
-}
-conn.sync({ force: true }).then(() => {
-  getAllDb()
-  app.listen(PORT || 8080, () => {
-    console.log(`server is running on port ${PORT}`)
-  })
+//     return data
+//   } catch (e) {
+//     console.log(e)
+//   }
+// }
+// conn.sync({ force: true }).then(() => {
+//   getAllDb()
+//   app.listen(PORT || 8080, () => {
+//     console.log(`server is running on port ${PORT}`)
+//   })
+// })
+
+app.listen(PORT || 8080, () => {
+  console.log(`server is running on port ${PORT}`)
 })
--- src/models/Activities.js
+++ src/models/Activities.js
@@ -1,34 +1,34 @@
-const { DataTypes, UUIDV4 } = require('sequelize')
-// Exportamos una funcion que define el modelo
-// Luego le injectamos la conexion a sequelize.
-module.exports = sequelize => {
-  // defino el modelo
-  sequelize.define(
-    'activity',
-    {
-      id: {
-        type: DataTypes.UUID,
-        defaultValue: DataTypes.UUIDV4,
-        allowNull: false,
-        primaryKey: true
-      },
-      name: {
-        type: DataTypes.STRING,
-        allowNull: false
-      },
-      difficulty: {
-        type: DataTypes.ENUM('1', '2', '3', '4', '5')
-      },
-      duration: {
-        type: DataTypes.STRING
-      },
-      season: {
-        type: DataTypes.ENUM('Verano', 'Otoño', 'Invierno', 'Primavera')
-        //type: DataTypes.STRING
-      }
-    },
-    {
-      timestamps: false
-    }
-  )
-}
+// const { DataTypes, UUIDV4 } = require('sequelize')
+// // Exportamos una funcion que define el modelo
+// // Luego le injectamos la conexion a sequelize.
+// module.exports = sequelize => {
+//   // defino el modelo
+//   sequelize.define(
+//     'activity',
+//     {
+//       id: {
+//         type: DataTypes.UUID,
+//         defaultValue: DataTypes.UUIDV4,
+//         allowNull: false,
+//         primaryKey: true
+//       },
+//       name: {
+//         type: DataTypes.STRING,
+//         allowNull: false
+//       },
+//       difficulty: {
+//         type: DataTypes.ENUM('1', '2', '3', '4', '5')
+//       },
+//       duration: {
+//         type: DataTypes.STRING
+//       },
+//       season: {
+//         type: DataTypes.ENUM('Verano', 'Otoño', 'Invierno', 'Primavera')
+//         //type: DataTypes.STRING
+//       }
+//     },
+//     {
+//       timestamps: false
+//     }
+//   )
+// }
