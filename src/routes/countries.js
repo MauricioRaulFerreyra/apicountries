4208584453fedb5415225c903a8d78737651cc88
MauricioRaulFerreyra <mauriciotauro82@hotmail.com>
2022-05-25 12:41:18 -0300

    prueba deploy heroku

--- src/routes/countries.js
+++ src/routes/countries.js
@@ -1,10 +1,10 @@
-const express = require('express')
-const router = express.Router()
+// const express = require('express')
+// const router = express.Router()
 
-const { getAll, getById } = require('../controllers')
+// const { getAll, getById } = require('../controllers')
 
-router.get('/', getAll)
+// router.get('/', getAll)
 
 // router.get('/:id', getById)
 
-module.exports = router
+// module.exports = router
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,14 @@
 const { Router } = require('express')
 
-const countryRouter = require('./countries')
-// const activityRouter = require('./activities')
+//const countryRouter = require('./countries')
+//const activityRouter = require('./activities')
 const router = Router()
 
-router.use('/countries', countryRouter)
-// router.use('/activities', activityRouter)
+//router.use('/countries', countryRouter)
+//router.use('/activities', activityRouter)
+
+router.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
 module.exports = router
