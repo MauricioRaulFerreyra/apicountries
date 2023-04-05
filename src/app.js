5f02e732ea34edc8bfe5e87e5909400bdb3ee5a4
MauricioRaulFerreyra <mauriciotauro82@hotmail.com>
2023-04-05 06:13:20 -0300

    se cambia el archivo db.js por db-pool.js

--- src/app.js
+++ src/app.js
@@ -1,17 +1,18 @@
-const express = require('express')
-const cookieParser = require('cookie-parser')
-const morgan = require('morgan')
-const routes = require('./routes/index.js')
-const cors = require('cors')
-require('./db.js')
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const morgan = require("morgan");
+const routes = require("./routes/index.js");
+const cors = require("cors");
+// require('./db.js')
+require("./db-pool.js");
 
-const app = express()
+const app = express();
 
-app.use(cors())
-app.use(express.json())
-app.use(cookieParser())
-app.use(morgan('dev'))
+app.use(cors());
+app.use(express.json());
+app.use(cookieParser());
+app.use(morgan("dev"));
 
-app.use('/', routes)
+app.use("/", routes);
 
-module.exports = app
+module.exports = app;
--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,89 +1,90 @@
-const { Country, Activity } = require('../db')
-const { Op } = require('sequelize')
+// const { Country, Activity } = require('../db')
+const { Country, Activity } = require("../db-pool.js");
+const { Op } = require("sequelize");
 
 module.exports = {
   getAll: function (req, res, next) {
-    let name = req.query.name
+    let name = req.query.name;
     if (name) {
       const country = Country.findAll({
         include: Activity,
         where: {
-          name: { [Op.iLike]: `%${name}%` }
-        }
-      })
+          name: { [Op.iLike]: `%${name}%` },
+        },
+      });
       country
-        .then(data => res.status(200).json(data))
-        .catch(err => res.status(404).json('no countries found', err))
+        .then((data) => res.status(200).json(data))
+        .catch((err) => res.status(404).json("no countries found", err));
     } else {
       let data = Country.findAll({
         include: {
           model: Activity,
-          atributes: ['name', 'difficulty', 'duration', 'season'],
+          atributes: ["name", "difficulty", "duration", "season"],
           through: {
-            atributes: []
-          }
-        }
-      })
+            atributes: [],
+          },
+        },
+      });
       data
-        .then(aux => res.status(200).json(aux))
-        .catch(err => res.status(404).json('not countries found', err))
+        .then((aux) => res.status(200).json(aux))
+        .catch((err) => res.status(404).json("not countries found", err));
     }
   },
 
   getById: function (req, res, next) {
-    let newId = req.params.id
+    let newId = req.params.id;
     if (newId) {
       let data = Country.findOne({
         include: Activity,
         where: {
-          id: { [Op.iLike]: `%${newId}%` }
-        }
-      })
+          id: { [Op.iLike]: `%${newId}%` },
+        },
+      });
       data
-        .then(aux => res.status(200).json(aux))
-        .catch(err => res.status(404).json('not country found', err))
+        .then((aux) => res.status(200).json(aux))
+        .catch((err) => res.status(404).json("not country found", err));
     } else {
-      console.log('not country found')
-      next(err)
+      console.log("not country found");
+      next(err);
     }
   },
 
   postActivity: async function (req, res, next) {
     try {
-      let { name, difficulty, duration, season, idCountry } = req.body
+      let { name, difficulty, duration, season, idCountry } = req.body;
       const data = await Activity.create({
         name,
         difficulty,
         duration,
-        season
-      })
-      console.log(data)
+        season,
+      });
+      console.log(data);
       const country = await Country.findAll({
         where: {
-          id: idCountry
+          id: idCountry,
         },
-        attributes: ['id']
-      })
-      console.log(country)
-      data.addCountry(country)
-      res.status(200).json(data)
+        attributes: ["id"],
+      });
+      console.log(country);
+      data.addCountry(country);
+      res.status(200).json(data);
     } catch (err) {
-      res.status(404).json('Activity do not created', err)
+      res.status(404).json("Activity do not created", err);
     }
   },
 
   getActivity: async function (req, res, next) {
     try {
       let allActivities = await Activity.findAll({
-        atributes: ['name', 'difficulty', 'duration', 'season'],
+        atributes: ["name", "difficulty", "duration", "season"],
         through: {
-          atributes: []
-        }
-      })
+          atributes: [],
+        },
+      });
 
-      return res.status(200).json(allActivities)
+      return res.status(200).json(allActivities);
     } catch (err) {
-      res.status(404).json('no se encontraron actividades', err)
+      res.status(404).json("no se encontraron actividades", err);
     }
-  }
-}
+  },
+};
