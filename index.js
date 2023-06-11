a3f02387feab721df2c809b698395896f1d478b3
MauricioRaulFerreyra <mauriciotauro82@hotmail.com>
2023-06-11 20:22:05 -0300

    se modifica el archivo index.js

--- index.js
+++ index.js
@@ -17,12 +17,7 @@ const options = {
   },
 };
 
-// try {
-//   const response = await axios.request(options);
-//   console.log(response.data);
-// } catch (error) {
-//   console.error(error);
-// }
+
 
 /**LLAMAMOS A LA API */
 const getAll = async () => {
@@ -67,48 +62,6 @@ const countriesTableLoad = async () => {
   });
 };
 
-// const dataInfo = async () => {
-//   try {
-//     const info = await axios.get("https://restcountries.com/v3/all");
-
-//     const data = await info.data.map((res) => {
-//       return {
-//         id: res.cca3,
-//         name: res.name.common && res.name.common,
-//         image: res.flags && res.flags.map((flag) => flag),
-//         continent: res.continents && res.continents.map((el) => el),
-//         capital: res.capital ? res.capital.map((el) => el) : ["no data"],
-//         subregion: res.subregion,
-//         area: res.area,
-//         population: res.population,
-//       };
-//     });
-
-//     return data;
-//   } catch (error) {
-//     console.log(error);
-//   }
-// };
-
-// const countriesTableLoad = async () => {
-//   const countries = await dataInfo();
-//   countries.map((el) => {
-//     Country.findOrCreate({
-//       where: { name: el.name },
-//       defaults: {
-//         id: el.id,
-//         name: el.name,
-//         image: el.img,
-//         continent: el.continent,
-//         capital: el.capital,
-//         subregion: el.subregion,
-//         area: el.area,
-//         population: el.population,
-//         maps: el.maps,
-//       },
-//     });
-//   });
-// };
 
 // Syncing all the models at once.
 conn.sync().then(() => {
@@ -119,22 +72,4 @@ conn.sync().then(() => {
   });
 });
 
-// conn.sync({alter:true})
-// (async () => {
-//   await conn.sync({ force: true })
 
-//   const info = await dataInfo() //info de la api
-
-//   try {
-//     const data = await Country.findAll() // data de la tabla
-//     if (!data.length) {
-//       await Country.bulkCreate(info) // llena la Db
-//     }
-//   } catch (error) {
-//     console.log(error)
-//   }
-
-//   server.listen(PORT || 3000, () => {
-//     console.log('%s listening at ', PORT || 3000) // eslint-disable-line no-console
-//   })
-// })()
