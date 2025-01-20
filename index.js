4a3f56f5ce99ec5aaa973a999d9ca2c1019ccfcf
Full Stack Developer <79989950+MauricioRaulFerreyra@users.noreply.github.com>
2025-01-19 21:36:34 -0300

    Update index.js

--- index.js
+++ index.js
@@ -13,49 +13,65 @@ const PORT = process.env.PORT || 4000
 const getAll = async () => {
   try {
     let response;
+    const axiosConfig = {
+      timeout: 5000, // Timeout de 5 segundos
+      headers: {
+        'Accept': 'application/json',
+        'Connection': 'keep-alive'
+      }
+    };
+
     try {
-      response = await axios("https://restcountries.com/v3.1/all/");
+      response = await axios.get("https://restcountries.com/v3.1/all", axiosConfig);
     } catch (err) {
       console.log("Error al obtener datos de la API, utilizando datos locales:", err.message);
+      return api.map(formatCountryData); // Usar datos locales si falla la API
     }
 
-    const data = (response && response.data ? response.data : api).map((res) => {
-      return {
-        id: res.cca3,
-        name: res.name.common,
-        img: res.flags && res.flags.png,
-        continent: res.continents ? res.continents : ['no data'],
-        capital: res.capital ? res.capital : ['no data'],
-        subregion: res.subregion,
-        area: res.area,
-        population: res.population
-      };
-    });
-
-    return data;
+    return response.data.map(formatCountryData);
   } catch (err) {
-    console.log(err);
+    console.error("Error en getAll:", err);
+    throw err; // Propagar el error para manejarlo en countriesTableLoad
   }
 };
 
+// Función auxiliar para formatear los datos del país
+const formatCountryData = (res) => ({
+  id: res.cca3,
+  name: res.name.common,
+  img: res.flags?.png,
+  continent: res.continents || ['no data'],
+  capital: res.capital || ['no data'],
+  subregion: res.subregion,
+  area: res.area,
+  population: res.population
+});
+
 const countriesTableLoad = async () => {
-  const countries = await getAll()
-  countries.map(el => {
-    Country.findOrCreate({
-      where: { name: el.name },
-      defaults: {
-        id: el.id,
-        name: el.name,
-        image: el.img,
-        continent: el.continent,
-        capital: el.capital,
-        subregion: el.subregion,
-        area: el.area,
-        population: el.population
-      }
-    })
-  })
-}
+  try {
+    const countries = await getAll();
+    const promises = countries.map(el => 
+      Country.findOrCreate({
+        where: { name: el.name },
+        defaults: {
+          id: el.id,
+          name: el.name,
+          image: el.img,
+          continent: el.continent,
+          capital: el.capital,
+          subregion: el.subregion,
+          area: el.area,
+          population: el.population
+        }
+      })
+    );
+    
+    await Promise.all(promises);
+    console.log('Países cargados exitosamente en la base de datos');
+  } catch (err) {
+    console.error('Error al cargar países en la base de datos:', err);
+  }
+};
 
 // Syncing all the models at once.
 conn.sync({ alter: true }).then(() => {
